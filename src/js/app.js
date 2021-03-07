02ea71c3691f5af2e1adc38274b9b271f079b9ec
Deizee <deizee@mail.ru>
2021-03-07 08:38:21 +0500

    change app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
-// import GameSaving from './GameSaving';
-// import GameSavingLoader from './GameSavingLoader';
+import GameSaving from './GameSaving';
+import GameSavingLoader from './GameSavingLoader';
+
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    return new GameSaving(saving);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+})();
